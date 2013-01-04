06951755e5a1b85353e27bff2a387dd27c522de4
Nestor Pestelos and Ramon Tayag <dev+nestor+ramon@aelogica.com>
2013-01-04 12:56:45 +0800

    [#39761291] Add check to ensure dummy is running

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,12 @@
 APP_ENV = 'test'
 require File.join(File.dirname(__FILE__), '..', 'config', 'initializer')
 require 'rspec'
+
+# check that dummy is running
+dummy_rb = File.join(File.dirname(__FILE__), 'dummy', 'dummy.rb')
+start_dummy_command = "ruby #{File.expand_path(dummy_rb)} -p 1234" 
+command = %Q{ps -ef | grep "#{start_dummy_command}" | grep -v grep}
+dummy_running = !`#{command}`.empty?
+unless dummy_running
+  fail "Dummy must be running. Run `#{start_dummy_command}`"
+end
